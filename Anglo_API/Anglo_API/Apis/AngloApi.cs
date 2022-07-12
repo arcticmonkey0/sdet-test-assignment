362c910b5b73bfe5b1c51f462015908c56bc3ad6
Vladyslav Kaliuzhnyi <Vladyslav.Kaliuzhnyi@thomsonreuters.com>
2022-07-12 14:09:36 +0300

    initialize api project

---
+++ Anglo_API/Anglo_API/Apis/AngloApi.cs
@@ -0,0 +1,14 @@
+﻿using Anglo_API.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anglo_API.Apis
+{
+    internal class AngloApi
+    {
+        internal Cars Cars { get; private set; }
+    }
+}
